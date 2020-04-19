5a742412f7e24580f4f38675414ee076b93be2ad
Daniel(SinitsinDI) <daniel@example.com>
2020-04-20 02:45:11 +0300

    svg: внес в модуль все функции для работы с SVG

--- main.cpp
+++ main.cpp
@@ -14,21 +14,6 @@ vector<double> input_numbers(size_t count)
     return result;
 }
 
-void
-find_minmax(vector<double> numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for(double number:numbers)
-    {
-        if(number>max)
-            max=number;
-        if(number<min)
-            min=number;
-    }
-}
-
-
 vector <size_t> make_histogram(vector <double> numbers,size_t bin_count,double min,double max)
 {
     vector<size_t> bins(bin_count);
@@ -87,84 +72,6 @@ void show_histogram_text(vector<size_t>bins)
     }
 }
 
-void
-svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-
-void
-svg_end()
-{
-    cout << "</svg>\n";
-}
-
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
-{
-    cout << "<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />";
-}
-
-void
-svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='"<<baseline<<"'>"<<text<<"</text>";
-
-}
-
-void
-show_histogram_svg(const vector<size_t>& bins)
-{
-    const size_t screen_width=80;
-    const size_t width=screen_width-4;
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    //svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
-    //svg_rect(TEXT_WIDTH, 0, bins[0] * BLOCK_WIDTH, BIN_HEIGHT);
-    double top = 0;
-    string stroke="black";
-    string fill="red";
-    size_t max_count = 0;
-    for (size_t count : bins)
-    {
-        if (count > max_count)
-        {
-            max_count = count;
-        }
-    }
-    const bool scaling_needed = max_count > width;
-
-    for (size_t bin : bins)
-    {
-        if (scaling_needed)
-        {
-            const double scaling_multiplier = (double)width / max_count;
-            bin = (size_t)(bin * scaling_multiplier);
-        }
-
-
-    }
-
-    for (size_t bin : bins)
-    {
-        const double bin_width = BLOCK_WIDTH * bin;
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,stroke,fill);
-        top += BIN_HEIGHT;
-    }
-
-    svg_end();
-}
 
 int main()
 {
