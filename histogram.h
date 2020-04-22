3a31ebcf03cacafba59bc984eed5e1ee96caae61
Daniel(SinitsinDI) <daniel@example.com>
2020-04-22 20:45:00 +0300

    fix: перенес функции из main.cpp в histogram.cpp

--- histogram.h
+++ histogram.h
@@ -4,4 +4,7 @@
 using namespace std;
 void find_minmax(const vector<double> numbers, double& min, double& max);
 double bin_opacity17(double bin, size_t max_count);
+vector<double> input_numbers(size_t count);
+vector <size_t> make_histogram(vector <double> numbers,size_t bin_count,double min,double max);
+void show_histogram_text(vector<size_t>bins);
 #endif // HISTOGRAM_H_INCLUDED
