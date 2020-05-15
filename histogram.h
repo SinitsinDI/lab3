d32abcab81df457d400f53dd203423f145a61070
Daniel(SinitsinDI) <daniel@example.com>
2020-05-15 16:13:26 +0300

    code: завершил выполнение пункта 3.1

--- histogram.h
+++ histogram.h
@@ -3,8 +3,12 @@
 #include <vector>
 #include <iostream>
 using namespace std;
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 void find_minmax(const vector<double>& numbers, double& min, double& max);
 vector<double> input_numbers(istream& in ,size_t count);
-vector <size_t> make_histogram(const vector <double>& numbers,size_t bin_count,double min,double max);
+vector <size_t> make_histogram(Input);
 void show_histogram_text(vector<size_t>bins);
 #endif // HISTOGRAM_H_INCLUDED
